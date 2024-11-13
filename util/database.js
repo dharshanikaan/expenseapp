3b6888a0b4719d22c44059f92832037192f15405
negha <neghashree96@gmail.com>
2024-11-13 22:35:25 +0530

    data

--- util/database.js
+++ util/database.js
@@ -1,10 +1,14 @@
-require('dotenv').config({ path: '../expenseapppassword/.env' });
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    port:process.env.DB_PORT,
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD, 
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        port: process.env.DB_PORT,
+    }
+);
 
 const User = require('../models/user')(sequelize, Sequelize.DataTypes);
 const Expense = require('../models/expense')(sequelize, Sequelize.DataTypes);
